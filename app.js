2dacb357d63bedea0afa026b29ea153971aa5a85
high-u <zen.crazyd@gmail.com>
2018-04-16 20:30:31 +0900

    hoge

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-var webclient = require("request");
-var os = require('os');
-var hostname = os.hostname();
-webclient.get({
-  url: "https://node-red-docker-fdelqzqlup.now.sh/api/heroku?id=123&host=" + hostname,
-  qs: {
-    testkey: "testvalue",
-    hoge: "hoge"
-  }
-}, function (error, response, body) {
-  console.log(body);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
